f0a5a0a0ea07c2ca84976c32aaf1e21bb5e06454
Chakalisuresh <121609226+Chakalisuresh@users.noreply.github.com>
2024-01-30 22:56:52 +0530

    Add files via upload

---
+++ ShoppingCartDemo/src/test/java/com/ShoppingCartDemo/ShoppingCartDemo/ShoppingCartDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ShoppingCartDemo.ShoppingCartDemo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
