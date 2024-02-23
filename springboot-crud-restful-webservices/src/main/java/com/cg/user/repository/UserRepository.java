d7c0191a6b1e0f3f231b3341302900576383976d
Chakalisuresh <121609226+Chakalisuresh@users.noreply.github.com>
2024-02-23 11:30:02 +0530

    commit

---
+++ springboot-crud-restful-webservices/src/main/java/com/cg/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cg.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
